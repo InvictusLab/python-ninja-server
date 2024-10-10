84a50e8188001825a95ebe8ae363e6c9b24989a5
joker <invictuslab001@gmail.com>
2024-10-10 22:30:46 +0800

    perf(invictus): local.py ALLOWED_HOSTS is set to *

--- config/settings/local.py
+++ config/settings/local.py
@@ -14,7 +14,7 @@ SECRET_KEY = env(
     default="qbvdZmjLTGWhGTER6YwCWmleHU4Ik0TKbNM5x6TEM3n5gh307YxoyvmWKozuZfC1",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]  # noqa: S104
+ALLOWED_HOSTS = ["*"]  # noqa: S104
 
 # CACHES
 # ------------------------------------------------------------------------------
